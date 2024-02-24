d8948f2f283502397711374046961df3e22e90d2
amirabenbouali <125030857+amirabenbouali@users.noreply.github.com>
2024-02-24 02:22:36 +0000

    Implemented SalesToManagement.java file.

--- src/Management/SalesToManagement.java
+++ src/Management/SalesToManagement.java
@@ -1,6 +1,48 @@
 package Management;
 
 public abstract class SalesToManagement {
-    
+    private String saleId;
+    private double totalAmount;
+    private String paymentMethod;
+    private LocalDateTime saleTime;
+
+    public SalesToManagement(String saleId, double totalAmount, String paymentMethod, LocalDateTime saleTime) {
+        this.saleId = saleId;
+        this.totalAmount = totalAmount;
+        this.paymentMethod = paymentMethod;
+        this.saleTime = saleTime;
+    }
+
+    public String getSaleId() {
+        return saleId;
+    }
+
+    public void setSaleId(String saleId) {
+        this.saleId = saleId;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public LocalDateTime getSaleTime() {
+        return saleTime;
+    }
+
+    public void setSaleTime(LocalDateTime saleTime) {
+        this.saleTime = saleTime;
+    }
 }
 
