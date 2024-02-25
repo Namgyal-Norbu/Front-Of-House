78cdc073e6211a86221195be741ff11064f8d095
Namgyal-Norbu <tenzin.norbu@city.ac.uk>
2024-02-25 12:44:33 +0000

    renamed variable

--- src/Management/BookingstoManagement.java
+++ src/Management/BookingstoManagement.java
@@ -4,20 +4,20 @@ package Management;
 public abstract class BookingstoManagement {
      private int bookingID; // unique identifier
      private String name; // the name of the person the booking is under
-     private String number; // the telephone number provided when making a booking 
+     private String phoneNumber; // the telephone number provided when making a booking
      private String type; // the type of booking (phone, walkin, online, ..., etc)
 
      /**
      * Constructor to initialise the BookingsToManagement
      * @param bookingID 
      * @param name
-     * @param number
+     * @param phoneNumber
      * @param type
      */
-    public BookingstoManagement(int bookingID, String name, String number, String type) {
+    public BookingstoManagement(int bookingID, String name, String phoneNumber, String type) {
         this.bookingID = bookingID;
         this.name = name;
-        this.number = number;
+        this.phoneNumber = phoneNumber;
         this.type = type;
     }
 
@@ -37,7 +37,7 @@ public abstract class BookingstoManagement {
      * Getter method that returns number
      * @return number
      */   
-    public String getNumber() { return number; }
+    public String getNumber() { return phoneNumber; }
     
     /**
      * Getter method that returns type
